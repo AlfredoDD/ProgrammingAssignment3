63348240542119f3b5580470be41f6e1dcccc6fd
Alfredo Domínguez <alfredo.dd@gmail.com>
2015-01-25 12:12:21 +0100

    Plot part
    incorpores information about plot part

--- ProgAssingment3.R
+++ ProgAssingment3.R
@@ -1,1 +1,11 @@
 #Programming Assignment 3
+outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+head(outcome)
+outcome[, 11] <- as.numeric(outcome[, 11])
+hist(outcome[, 11])
+print("Number of NAs in this column")
+nas <- sum(is.na(outcome[,11]))
+nas
+print("Quality of colum as nrow-nas/nrow")
+quality <-(nrow(outcome)-nas)/nrow(outcome)
+quality
